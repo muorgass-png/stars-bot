11308624c593caf8d53a4443ab052f2b2f24f0e3
murodbekPHYTON <murodbekcrazy@gmail.com>
2026-06-06 15:42:53 +0500

    first commit

---
+++ config.py
@@ -0,0 +1,17 @@
+# =============================================
+#   SOZLAMALAR
+# =============================================
+
+BOT_TOKEN = "8945603893:AAExJwtZNrpNetcS08-eC30wqQgfYECSlY8"
+
+ADMIN_IDS = [7954682778]
+
+STARS_PER_REFERRAL = 2
+
+MIN_WITHDRAW = 15
+
+STAR_PRICE_UZS = 500
+
+DB_PATH = "stars_bot.db"
+
+CHANNEL_ID = -1003392716478
